87de09df0c942d4d0258bd655816782b26e06785
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2026-02-02 11:20:30 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,11 @@ import forms from '@tailwindcss/forms'
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-    
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
-        './resources/**/*.jsx',
-        './resources/**/*.tsx',
-    
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
         './node_modules/flowbite/**/*.js',
         './node_modules/flowbite-vue/**/*.js',
     ],
