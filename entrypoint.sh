82b469746b43bc896323e0c99e7e587930a4536c
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2026-02-02 11:10:15 +0800

    fix: vite tailwind asset mounting

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 php artisan migrate --force
 php artisan db:seed --force
 
+php artisan optimize:clear
+
 php artisan optimize
 
 exec php-fpm -F -R
