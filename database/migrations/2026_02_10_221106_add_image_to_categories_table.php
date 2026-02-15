0e9be48c3ae444df93cc58ed81a72f2a806f4627
Carlo1437 <camarote757@gmail.com>
2026-02-15 16:26:35 +0800

    fix: nullable image column

--- database/migrations/2026_02_10_221106_add_image_to_categories_table.php
+++ database/migrations/2026_02_10_221106_add_image_to_categories_table.php
@@ -9,11 +9,11 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up(): void 
     {
         Schema::table('categories', function (Blueprint $table) {
             //
-            $table->string('image')->after('name');;
+            $table->string('image')->nullable()->after('name');
         });
     }
 
